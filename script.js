6c9b3991b3f91cbd28b59cb81ce9e9bf5f930ee9
unknown <kirkosmail@gmail.com>
2022-08-17 00:51:35 +0300

    changed fonts to GoogleFonts, disable smoothlyJS

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let phrases = [
-    { image: 'img/img1-1.png' },
     { image: 'img/img2-1.png' },
     { image: 'img/img3-1.png' },
+    { image: 'img/img1-1.png' },
     { image: 'img/img4-1.png' },
     { image: 'img/img5-1.png' },
+    { image: 'img/img6-1.png' },
 ];
 
 function getRandomElement(arr) {
@@ -15,9 +16,9 @@ let mainimage = document.querySelector('.mainimage');
 
 mainimage.addEventListener('click', function () {
     let randomElement = getRandomElement(phrases);
-    smoothly(mainimage, 'src', randomElement.image);
+    mainimage.src = randomElement.image;
 });
 
 for (let i = 0; i <= 2; i = i + 1) {
-    smoothly(mainimage, 'src', phrases[i].image);
+        mainimage.src = phrases[i].image;
 }
